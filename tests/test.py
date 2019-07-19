7b8438f2096a78fc37da0682ccbc2bdf023c9b2e
Ben Comer <ben.comer@gatech.edu>
2019-07-19 17:18:49 -0400

    modified tests

--- tests/test.py
+++ tests/test.py
@@ -5,8 +5,8 @@ import numpy as np
 
 atoms = molecule('H2O')
 atoms[0].position += np.array([0,0,0.1])
-"""
-calc = Psi4(atoms = atoms,xc = 'ccsd(t)', basis = '6-31g**')
+
+calc = Psi4(atoms = atoms,xc = 'mp2', basis = '6-31g**')
 
 
 atoms.set_calculator(calc)
@@ -16,6 +16,7 @@ print(calc.parameters)
 # test energy call
 print(atoms.get_potential_energy())
 
+"""
 calc = Psi4('psi4-calc')
 print(calc.directory)
 print(calc.parameters)
@@ -27,7 +28,7 @@ relax.run(fmax = 0.05)
 energy = calc.psi4.energy('b3lyp/cc-pvdz',
                    molecule = calc.molecule,
                    return_wfn = False)
-"""
+
 # test more obscure settings
 calc = Psi4(atoms = atoms,
             method = 'b3lyp',
@@ -42,3 +43,4 @@ calc.calculate()
 
 calc.psi4.frequency('scf/cc-pvdz', molecule=calc.molecule, 
                     return_wfn=True, dertype=1)
+"""
