c227f70c04862a8814a58f64f0e3322158a14364
Ben Comer <ben.comer@gatech.edu>
2019-07-22 10:46:13 -0400

    fixed charge/mulitiplicty arguments and added more testing

--- tests/test.py
+++ tests/test.py
@@ -15,8 +15,10 @@ print(calc.parameters)
 
 # test energy call
 print(atoms.get_potential_energy())
+# test force call
+print(atoms.get_forces())
 
-"""
+
 calc = Psi4('psi4-calc')
 
 print(calc.parameters)
@@ -24,10 +26,62 @@ print(calc.parameters)
 relax = BFGSLineSearch(atoms)
 relax.run(fmax = 0.05)
 
+
+# test mutliplicity and charge kwargs
+
+atoms = molecule('H2')
+
+calc = Psi4(atoms = atoms,
+            charge = 1,
+            multiplicity =  2,
+            reference = 'uhf')
+print(atoms.get_potential_energy())
+
+
+del atoms[1]
+
+calc = Psi4(atoms = atoms,
+            multiplicity = 2,
+            reference = 'uhf')
+print(atoms.get_potential_energy())
+
+
+atoms = molecule('OH')
+
+calc = Psi4(atoms = atoms,
+            charge = -1,
+            reference = 'uhf')
+print(atoms.get_potential_energy())
+
+# test symmetry inputs
+atoms = molecule('H2O')
+calc = Psi4(atoms = atoms,
+            method = 'mp2',
+            basis = '6-31g**',
+            symmetry = 'c2v',
+            num_threads = 'max',
+            memory = '500MB',
+            reference = 'uhf')
+
+print(atoms.get_potential_energy())
+
 # test direct interaction with psi4
 energy = calc.psi4.energy('b3lyp/cc-pvdz',
                    molecule = calc.molecule,
                    return_wfn = False)
+
+# test warnings for inputs that psi4 doesn't take
+
+calc = Psi4(atoms = atoms,
+            method = 'cis',
+            smearing = 0.1,
+            kpts = (1,1,1),
+            nbands = 3,
+            reference = 'uhf')
+
+print(atoms.get_potential_energy())
+
+
 
 # test more obscure settings
 calc = Psi4(atoms = atoms,
@@ -43,4 +97,5 @@ calc.calculate()
 
 calc.psi4.frequency('scf/cc-pvdz', molecule=calc.molecule, 
                     return_wfn=True, dertype=1)
-"""
+
+
